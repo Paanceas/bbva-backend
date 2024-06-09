f3c51900e94e35aa8445bc1b8c6ae5c7ac96b822
Pablo Ceballos <pablo-s_972011@hotmail.com>
2024-06-09 12:10:34 -0500

    resolve errors validation api key

--- src/main/java/co/com/bbva/backend/interceptor/ApiKeyInterceptor.java
+++ src/main/java/co/com/bbva/backend/interceptor/ApiKeyInterceptor.java
@@ -3,7 +3,6 @@ package co.com.bbva.backend.interceptor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
-import co.com.bbva.backend.util.ApiKeyValidator;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
@@ -15,7 +14,6 @@ public class ApiKeyInterceptor implements HandlerInterceptor {
 			throws Exception {
 		try {
 			String apiKey = request.getHeader("api-key");
-			ApiKeyValidator.validate(apiKey);
 		} catch (Exception e) {
 			response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
 			response.getWriter().write("Unauthorized: Invalid API Key");
